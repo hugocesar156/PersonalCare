54eb288118b17338b8ffb53f2e2b65e960e349b6
Hugo César <hugocesar802@gmail.com>
2024-05-18 16:43:33 -0300

    Criação de classe base de repository.

---
+++ PersonalCare/PersonalCare.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalCare.DAL.Context;
+
+namespace PersonalCare.DAL.Repositories
+{
+    public class BaseRepository
+    {
+        protected static void SetConnectionString(DataContextBase data, string idEmpresa)
+        {
+            data.Database.SetConnectionString(data.Database.GetConnectionString()?.Replace("db_personalcare_base", $"db_personalcare_-{idEmpresa}"));
+        }
+    }
+}
